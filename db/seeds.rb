2e528ee80ac2392b9a5230b1231493aec126c4ff
sugi henson <sugihenson@gmail.com>
2024-12-04 16:59:33 +0800

    created tasks#new  and tasks#create, began working on new habit button

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,10 @@ eva = User.create!(email: "eva@dashboard.com", password: "1234567", first_name: 
 nini = User.create!(email: "nini@dashboard.com", password: "1234567", first_name: "Nini")
 fra = User.create!(email: "fra@dashboard.com", password: "1234567", first_name: "Fra")
 
-habit_widget = Widget.create!(
-category: "habit",
-position_x: 0,
-position_y: 0,
-width: 2,
-height: 2,
-user: sugi)
+habit_widget = Widget.create!( category: "habit", position_x: 0, position_y: 0, width: 2, height: 2, user: sugi)
 
 Task.create!(title: "read", done: false, widget: habit_widget)
+Task.create!(title: "meditate", done: true, widget: habit_widget)
+Task.create!(title: "exercise", done: false, widget: habit_widget)
+Task.create!(title: "floss", done: true, widget: habit_widget)
+Task.create!(title: "journal", done: false, widget: habit_widget)
