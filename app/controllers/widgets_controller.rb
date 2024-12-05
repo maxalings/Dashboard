94e98f5c7d9b5ba24e2dd65cc33808257fb34ca6
max alings <alings.max@gmail.com>
2024-12-05 16:45:07 +0800

    a lot

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -2,6 +2,7 @@ require 'open-uri'
 require 'json'
 
 class WidgetsController < ApplicationController
+
   def new
     @widget = Widget.new
   end
@@ -25,5 +26,19 @@ class WidgetsController < ApplicationController
     @widget = Widget.find(params[:widget_id])
     @stock = Stock.new
     @stocks = Stock.all
+
+    @total_value = @stocks.map do |stock|
+      stock.amount * stock.current_price
+    end.sum
+
+    @total_invested = @stocks.map do |stock|
+      stock.amount * stock.purchase_price
+    end.sum
+    if @total_invested > 0
+      @percentage_change = ((@total_value - @total_invested) / @total_invested) * 100
+    else
+      @percentage_change = 0
+    end
+    @percentage_change = @percentage_change.round(2)
   end
 end
