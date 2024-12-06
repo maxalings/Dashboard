6fc56949a4469244642cfcadfeb132dfa03955d0
Franck Christmas <54951843+FranckChristmas@users.noreply.github.com>
2024-12-07 00:20:18 +0800

    save before WE

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -1,2 +1,12 @@
 class FlashcardsController < ApplicationController
+
+def create
+  @flashcard = Flashcard
 end
+
+
+end
+
+# def new
+#   @task = Task.new
+#   @widget = Widget.find(params[:widget_id])
