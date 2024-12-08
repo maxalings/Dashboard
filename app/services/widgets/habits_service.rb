2359f29c6df61aaf0992e8a724627f9c1edb76e1
sugi henson <sugihenson@gmail.com>
2024-12-08 17:58:06 +0800

    attempted to create user_tz in habits_service. not functional yet but does not break anythng

--- app/services/widgets/habits_service.rb
+++ app/services/widgets/habits_service.rb
@@ -1,3 +1,6 @@
+require 'tzinfo'
+require 'geocoder'
+
 class Widgets::HabitsService
   attr_reader :tasks, :widget
 
@@ -6,7 +9,22 @@ class Widgets::HabitsService
     @tasks = @widget.tasks
   end
 
+  def user_tz
+    #  determine time zone using current_sign_in_ip from devise
+    user_ip = widget.user.current_sign_in_ip
+    user_location = Geocoder.search(user_ip).first
+    if user_location
+      tf = TimezoneFinder.create
+      timezone_name = tf.timezone_at(lng:user_location.data["center"][0], lat: user_location.data["center"][1])
+      TZInfo::Timezone.get(timezone_name)
+    else
+      "UTC"
+    end
+  end
+
   def first_session_today?
+    # Need to convert the following values into users time zone using
+    #  .in_time_zone(user_tz)
     last_sign_in = widget.user.current_sign_in_at
     previous_sign_in = widget.user.last_sign_in_at
 
@@ -14,9 +32,7 @@ class Widgets::HabitsService
     puts "Last sign in: #{last_sign_in}"
     puts "Previous sign in: #{previous_sign_in}"
 
-    # If there's no previous sign in, this must be their first session
-    # this doesn't work because devise always records last_sign_in_at and
-    # current_sign_in_at as the same date
+    # If previous sign-in and last sign-in are the same, this must be their first session:
     return true if previous_sign_in == last_sign_in
     # If the last sign in was today, and the previous sign in was not today, this must be their first session today:
     last_sign_in.to_date == Date.current && previous_sign_in.to_date != Date.current
