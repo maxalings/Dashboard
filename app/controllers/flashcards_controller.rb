a10cef578b3962dfa3affd3d13fec781b3852c8b
Franck Christmas <54951843+FranckChristmas@users.noreply.github.com>
2024-12-09 09:54:16 +0800

    last changes

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -3,10 +3,9 @@ class FlashcardsController < ApplicationController
 def create
   @flashcard = Flashcard
 end
-
-
-end
-
 # def new
 #   @task = Task.new
 #   @widget = Widget.find(params[:widget_id])
+
+end
+
