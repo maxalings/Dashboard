fbc23818fce09047b2aa829d371bd8945669a02c
Nini S <ninashen1998@gmail.com>
2024-12-10 15:18:38 +0800

    bidget styling fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -62,4 +62,5 @@ Flashcard.create!(question: "what is the capital of Indonesia ?", answer: "Jakar
 
 Widget.create!( category: "time", user: eva, position_x: 0, position_y: 0, timezone: "new york")
 
-# Race.create!(day: 12, month: 1, year: 2025, widget: fitness_widget)
+Race.create!(day: 12, month: 1, year: 2025, widget: fitness_widget)
+Race.create!(day: 1, month: 2, year: 2025, widget: fitness_widget)
