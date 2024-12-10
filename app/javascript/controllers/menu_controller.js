c5fbc9e46a3cb01aef5422b1eca5954ca573cb37
Eva Orlandini <evaorlandini.pro@gmail.com>
2024-12-10 12:03:10 +0800

    move icon on bottom rigth

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["checkbox"]; // Définit les targets pour les checkboxes
+  static targets = ["checkbox", "profile", "widgets"]; // Définit les targets pour les checkboxes
 
   connect() {
   }
@@ -14,5 +14,15 @@ export default class extends Controller {
         checkbox.checked = false; // Décoche toutes les autres checkboxes
       }
     });
+  }
+
+  floating(event) {
+    this.profileTarget.classList.add("animate-float-1");
+    this.widgetsTarget.classList.add("animate-float-2");
+  }
+
+  stopfloating(event) {
+    this.profileTarget.classList.remove("animate-float-1");
+    this.widgetsTarget.classList.remove("animate-float-2");
   }
 }
