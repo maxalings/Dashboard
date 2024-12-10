7d94cf690274cf656f080c0fd2148da5a1fc1d7c
Nini S <ninashen1998@gmail.com>
2024-12-10 11:40:50 +0800

    minidget stlying weired when relog in

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -5,27 +5,15 @@ class RacesController < ApplicationController
   end
 
   def create
+    p params
     @race = Race.new(race_params)
     @race.widget = Widget.find(params[:widget_id])
-
-    given_date = Time.new(params[:year].to_i, params[:month].to_i, params[:day].to_i, 0, 0, 0)
-    now = Time.now
-    total_seconds = (given_date - now).to_i
-
-    if total_seconds.positve?
-      @days = total_seconds / (60 * 60 * 24)
-      @hours = (total_seconds % (60 * 60 * 24)) / (60 * 60)
-      @minutes = (total_seconds % (60 * 60)) / 60
-      @seconds = total_seconds % 60
-    else
-      @days = @hours = @minutes = @seconds = 0
-    end
-
     @race.save!
-    # redirect_to widgets_path(current_user.id)
+    redirect_to widgets_path
+
   end
 
   def race_params
-    params.require(:race).permit(:day, :month, :year)
+    params.permit(:day, :month, :year, :widget_id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -63,4 +63,4 @@ Flashcard.create!(question: "what is the capital of Indonesia ?", answer: "Jakar
 
 Widget.create!( category: "time", user: eva, position_x: 0, position_y: 0, timezone: "new york")
 
-Race.create!(day: 12, month: 11, year: 2025, widget: fitness_widget)
+Race.create!(day: 12, month: 1, year: 2025, widget: fitness_widget)
